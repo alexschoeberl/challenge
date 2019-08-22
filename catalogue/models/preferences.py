afb1a63714f3f9f25f213fb5f126260e0be9b681
Alexander Schöberl <alexander.schoeberl@gmail.com>
2019-08-22 14:20:00 +0200

    Add preference resource

--- catalogue/models/preferences.py
+++ catalogue/models/preferences.py
@@ -2,19 +2,19 @@ from catalogue import database as db
 
 
 class Preference(db.Model):
-	__tablename__ = 'Preferences'
+    __tablename__ = 'Preferences'
 
-	book_idf = db.Column(db.Integer, db.ForeignKey('Books.idf'), primary_key=True)
-	book = db.relationship('Book', backref=db.backref('preferences', cascade='all, delete-orphan'), uselist=False)
-	char_min = db.Column(db.Integer, default=0, nullable=False)
-	char_max = db.Column(db.Integer)
+    book_idf = db.Column(db.Integer, db.ForeignKey('Books.idf'), primary_key=True)
+    book = db.relationship('Book', backref=db.backref('preferences', cascade='all, delete-orphan'), uselist=False)
+    char_min = db.Column(db.Integer, default=0, nullable=False)
+    char_max = db.Column(db.Integer)
 
-	def serialize(self):
-		return {
-			'book': self.book,
-			'char_min': self.char_min,
-			'char_max': self.char_max
-		}
+    def serialize(self):
+        return {
+            'book': self.book,
+            'char_min': self.char_min,
+            'char_max': self.char_max
+        }
 
-	def __repr__(self):
-		return f'<Preference {self.book.idf}>'
+    def __repr__(self):
+        return f'<Preference {self.book.idf}>'
